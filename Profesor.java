daa03850cba7d2ef81d50e31064323545e60f678
Cristiammontoya <138298152+Cristiammontoya@users.noreply.github.com>
2026-09-21 13:13:18 -0500

    Update Profesor.java

--- Profesor.java
+++ Profesor.java
@@ -7,6 +7,33 @@ public class Profesor {
     private String nombre;
     private String especialidad;
     private double salario;
-    private Departamento departamento;
-    private List<Curso> cursos;
+    private Departamento departamento; 
+    private List<Curso> cursosImpartidos;
+
+    public Profesor(String nombre, String id, String especialidad, double salario) {
+        this.nombre = nombre;
+        this.id = id;
+        this.especialidad = especialidad;
+        this.salario = salario;
+        this.cursosImpartidos = new ArrayList<>();
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public String getNombre() { return nombre; }
+    public String getId() { return id; }
  }
