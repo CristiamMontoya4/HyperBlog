0406458601fe991d186c5c3dc0b72f9429947111
Cristiammontoya <138298152+Cristiammontoya@users.noreply.github.com>
2026-09-21 13:18:00 -0500

    Update Estudiante.java

--- Estudiante.java
+++ Estudiante.java
@@ -2,9 +2,24 @@ package entities;
 
 
 public class Estudiante {
-    private double id;
+    private String id;
     private String nombre;
     private String carrera;
     private int semestre;
-    private Universidad universidad;
+    private List<Curso> cursosMatriculados;
+
+    public Estudiante(String nombre, String id, String carrera, int semestre) {
+        this.nombre = nombre;
+        this.id = id;
+        this.carrera = carrera;
+        this.semestre = semestre;
+        this.cursosMatriculados = new ArrayList<>();
+    }
+
+    public List<Curso> getCursosMatriculados() {
+        return cursosMatriculados;
+    }
+
+    public String getNombre() { return nombre; }
+    public String getId() { return id; }
 }
